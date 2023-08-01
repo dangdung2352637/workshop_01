bc1b35bcada5b70f565e6b72b1555b2e5d1c22f5
dung5559 <maibennhau534@gmail.com>
2023-08-01 11:52:45 +0700

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.7"
+      },
+      {
+        version: "0.8.0"
+      },
+      {
+        version: "0.6.12"
+      }
+    ],
+    networks: {
+      bsctest: {
+        url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+        accounts: [process.env.PRIV_KEY]
+      }
+    }
+  }
+
+};
