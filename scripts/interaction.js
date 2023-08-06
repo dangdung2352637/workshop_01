e79096c006738b7c65231325d4226c1358d86032
dung5559 <maibennhau534@gmail.com>
2023-08-06 11:27:45 +0700

    2

--- scripts/interaction.js
+++ scripts/interaction.js
@@ -4,8 +4,8 @@ const Web3 = require('web3');
 const floppyAbi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burnFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 const floppyAddress = "0x69b40D07fbd3C8c60C39783706C4ad069a251F76";
 myPrivateKey = process.env.PRIV_KEY;
-myAddress = "0x06265d833e09acaCEeAF0E64F9689c201cb36217";
-receiverAddress = "0x2A63490105ED2262EB268eFB12160D800ED573c7";
+myAddress = "0xFD3A99c87673248651Bff32fbFbf082272E62e31";
+receiverAddress = "0x06265d833e09acaCEeAF0E64F9689c201cb36217";
 
 async function interact(){
     web3 = await new Web3('https://data-seed-prebsc-2-s2.binance.org:8545');
@@ -13,23 +13,29 @@ async function interact(){
 
     //call function from contract : call , send
 
-    myBalance = await floppyContract.methods.balanceOf(myAddress).call();
-    console.log(myBalance);
+    // myBalance = await floppyContract.methods.owner().call();
+    // console.log(myBalance);
     //transfer
+    var a = web3.utils.toWei('6', 'gwei')
+    console.log("a: ",a);
+    await web3.eth.accounts.wallet.add(myPrivateKey);
     // await web3.eth.accounts.wallet.add(myPrivateKey);
-    // // await web3.eth.accounts.wallet.add(myPrivateKey);
-    // receiverBalanceBefore = await floppyContract.methods.balanceOf(receiverAddress).call();
-    // // console.log(receiverBalanceBefore);
+    
+    receiverBalanceBefore = await floppyContract.methods.balanceOf(receiverAddress).call();
+    console.log("receiverBalance",receiverBalanceBefore);
+    
+    rs = await floppyContract.methods.transfer(receiverAddress,222222222 ).send({
+        from: myAddress,
+        gas: 200000,
+        gasPrice: web3.utils.toWei('10', 'gwei')
+    });
+    
 
-    // rs = await floppyContract.methods.transfer(receiverAddress, 100000).send({
-    //     from : myAddress,
-    //     gas : 300000
-    // });
-
-    // receiverBalanceAfter = await floppyContract.methods.balanceOf(receiverAddress).call();
+    receiverBalanceAfter = await floppyContract.methods.balanceOf(receiverAddress).call();
 
 
-    // console.log(rs , receiverBalanceBefore , receiverBalanceAfter);
+    console.log(rs , receiverBalanceBefore , receiverBalanceAfter);
 }
+
 
 interact();
